2e350482494190a573774954917248988fd6361d
Mac Pro <macpro@192.168.1.50>
2024-11-10 15:51:43 +0100

    add typing animation functionality to subtitle in about section

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+
+//initialized and typed object from a typed class for text typing animation
+var typed = new Typed('#animatedSubtitleTyping', {
+    strings: ['ORGANISATION.', 'PROJECT.','COMPANY.','EDUCATION.'],
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 30,
+    backDelay: 1500,
+    
+  });
+
