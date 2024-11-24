cd735f07156f93f77e34f1eab6aa4a661cac4fed
Mac Pro <macpro@Macs-MacBook-Pro.local>
2024-11-24 19:35:18 +0100

    forgot to remove local ports and use app exportation from last commit, now it's fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 //declare a port to use for the server
-const port = 3000; //for local testing
+//const port = 3000; //for local testing
 
 // Set up EJS as the view engine
 app.set("view engine", "ejs");
@@ -31,10 +31,10 @@ app.get("/", (req, res) => {
 });
 
 // Export the app for Vercel
-//export default app; //for vercel deployment
+export default app; //for vercel deployment
 
 
 //listening to the declared port locally
-app.listen(port,()=>{
-console.log("Listening to the port : "+port);
-});
+//app.listen(port,()=>{
+//console.log("Listening to the port : "+port);
+//});
