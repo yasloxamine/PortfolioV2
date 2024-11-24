af95043d6c3e69fa1fb12ab787c284f355a4fb6c
Mac Pro <macpro@Macs-MacBook-Pro.local>
2024-11-24 19:33:31 +0100

    try first fix by renaming styles folder and file to css folder and styles.css, this potential fixes are not improvised, they are researched, also i'm committing these changes to github for vercel deployment purposes

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 //declare a port to use for the server
-//const port = 3000; //for local testing
+const port = 3000; //for local testing
 
 // Set up EJS as the view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); // Ensure the views folder is correctly located
 
 // Assign the public folder for static assets (CSS, images, etc.)
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,10 +31,10 @@ app.get("/", (req, res) => {
 });
 
 // Export the app for Vercel
-export default app; //for vercel deployment
+//export default app; //for vercel deployment
 
 
 //listening to the declared port locally
-//app.listen(port,()=>{
-//console.log("Listening to the port : "+port);
-//});
+app.listen(port,()=>{
+console.log("Listening to the port : "+port);
+});
